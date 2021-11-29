fccba535441c4a814aec58ff97b0d891b9d34c2b
djibadiawara <djiba.diawara97@gmail.com>
2021-11-29 15:37:07 -0500

    commit tree 5d195c9d5ad653a6a24b52fcdbd31024672fe931

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(resp => resp.json())
+  .then(books => renderBooks(books)) 
+  }
+
+
   
-}
+// function renderBooks(books) {
+//   const main = document.querySelector('main');
+//   books.forEach(book => {
+//     const h2 = document.createElement('h2');
+//     h2.innerHTML = book.name;
+//     main.appendChild(h2);
+//   });
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   fetchBooks(); 
+ 
+// });
+
+// function fetchBooks() {
+//   // To pass the tests, don't forget to return your fetch!
+//   fetch('https://anapioficeandfire.com/api/books')
+//   .then(resp => resp.json())
+//   .then(books.forEach(book => renderBooks))
+// }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -12,6 +37,10 @@ function renderBooks(books) {
   });
 }
 
+
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks()
+
 });
+
+
